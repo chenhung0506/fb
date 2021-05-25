40c58d2b278eb2e2bdc6c30a1c280c6a743e6683
chenhung lin <>
2021-05-25 22:28:08 +0800

    upgrade

--- module/server.py
+++ module/server.py
@@ -50,10 +50,15 @@ def webhook():
         text = data['entry'][0]['messaging'][0]['message']['text'] 
         log.info(text)
         log.info(sender)
+        payload = {'recipient': {'id': sender}, 'message': {'text': "您好,感謝您與我們聯繫!如有問題請您先參考下方智慧櫃檯功能選項："}}
+        r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
         payload = {'recipient': {'id': sender}, 'message': json.loads(const.PAYLOAD)}
         log.info(payload)
         r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
-        payload = {'recipient': {'id': sender}, 'message': {'text': "您好,感謝您與我們聯繫!如有問題請您先參考下方智慧櫃檯功能選項："}}
+
+        payload = {'recipient': {'id': sender}, 'message': {"attachment":{"type":"template","payload":{"template_type":"button","text":"還有其他問題需要幫忙嗎？請點選下方按鈕由小幫手嗶寶協助您","buttons":[{"type":"web_url","url":"https://www.messenger.com","title":"小幫手嗶寶"}]}}}}
+        r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
+        payload = {'recipient': {'id': sender}, 'message': {'text': "或是您可以撥打本公司客服中心：412-8880按1後再按7轉接專員(手機及金馬地區請加02)。"}}
         r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
 
     except Exception as e:
